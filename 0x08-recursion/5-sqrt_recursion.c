2238649dfd3f06fd85ec45af5ee83b4710e958e0
Youssef Ramadan <ramadan176@icloud.com>
2023-10-25 14:18:35 -0700

    Break

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,6 +14,7 @@ int _sqrt_recursion(int n)
 		return (-1);
 	return (actual_sqrt_recursion(n, 0));
 }
+
 /**
  * actual_sqrt_recursion - recurses to find the natural
  * square root of a number
@@ -26,7 +27,7 @@ int actual_sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
 		return (-1);
-	if (i * i == 0)
+	if (i * i == n)
 		return (i);
 	return (actual_sqrt_recursion(n, i + 1));
 }
