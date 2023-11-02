fd36d7156a87e6c517f6ef7860ed73000da6b4e3
Youssef Ramadan <ramadan176@icloud.com>
2023-11-01 19:15:03 -0700

    haaaaa

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,57 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
-* main - prints the min number of coins to make change
-* for an amount of money
-* @argc: argument count
-* @argv: arguments
-* Return: 0
-*/
+ * main - prints the minimum number of coins to make change
+ * for an amount of money
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0 for success, 1 for error
+ */
 int main(int argc, char **argv)
 {
-int total, count;
-unsigned int i;
-char *p;
-int cents[] = {25, 10, 5, 2};
+	int total, count;
+	unsigned int i;
+	char *p;
+	int cents[] = {25, 10, 5, 2};
 
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
+	total = strtol(argv[1], &p, 10);
+	count = 0;
+
+	if (!*p)
+	{
+		while (total > 1)
+		{
+			for (i = 0; i < sizeof(cents) / sizeof(cents[0]); i++)
+			{
+				if (total >= cents[i])
+				{
+					count += total / cents[i];
+					total = total % cents[i];
+				}
+			}
+		}
+		if (total == 1)
+			count++;
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", count);
+	return (0);
 }
-
-
-total = strtol(argv[1], &p, 10);
-count = 0;
-
-
-if (!*p)
-{
-while (total > 1)
-{
-for (i = 0; i < sizeof(cents[i]); i++)
-{
-   if (total >= cents[i])
-   {
-       count += total / cents[i];
-       total = total % cents[i];
-   }
-}
-}
-if (total == 1)
-count++;
-}
-else
-{
-printf("Error\n");
-return (1);
-}
-
-
-printf("%d\n", count);
-return (0);
-}
-
