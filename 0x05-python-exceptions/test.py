6faf845bf51c376994a587e45c05e9473f4d1cbf
yousseframadan <yousseframadan@gmail.com>
2024-04-01 15:07:20 -0700

    ha

---
+++ 0x05-python-exceptions/test.py
@@ -0,0 +1,10 @@
+try:
+    fo2 = int(input("enter value fo2:")
+    ta7t = int(input("enter value ta7t:")
+    result = fo2 / ta7t
+
+except ValueError:
+    print("value error: alooo ezay ya3ni")
+
+except ZeroDivisionError:
+    print("mosta7el yasta fala777777")
